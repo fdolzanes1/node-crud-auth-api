e63c02a49dd4aa530121c56b14d962665b5dfca1
fdolzanes1 <fdolzanes1@gmail.com>
2022-08-03 11:33:02 -0400

    facilitando o uso do statusCode

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
-
+import { StatusCodes } from 'http-status-codes';
 
 const usersRouter = Router();
 
 usersRouter.get('/users', (req, res, next) => {
   const users = [{ userName: 'John', email: 'john@mail.com' }];
-  res.status(200).send({ users });
+  res.status(StatusCodes.OK).send({ users });
 });
 
 usersRouter.get('/users/:uuid', (req, res, next) => {
   const uuid = req.params.uuid;
-  res.status(200).send({ "uuid": uuid});
+  res.status(StatusCodes.OK).send({ "uuid": uuid});
 });
 
 export default usersRouter;
