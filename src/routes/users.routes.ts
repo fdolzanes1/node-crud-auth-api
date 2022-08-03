428fcee50a6fd040e788ffdf1b3b2f231ae6b7ca
fdolzanes1 <fdolzanes1@gmail.com>
2022-08-03 11:25:09 -0400

    implementando rota por id de usuario

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,9 +3,14 @@ import { Router } from "express";
 
 const usersRouter = Router();
 
-usersRouter.get('/', (req, res, next) => {
+usersRouter.get('/users', (req, res, next) => {
   const users = [{ userName: 'John', email: 'john@mail.com' }];
   res.status(200).send({ users });
+});
+
+usersRouter.get('/users/:uuid', (req, res, next) => {
+  const uuid = req.params.uuid;
+  res.status(200).send({ "uuid": uuid});
 });
 
 export default usersRouter;
